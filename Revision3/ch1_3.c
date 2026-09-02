c2f07211d515e0716f45852589bdc53714bd2237
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-02 18:51:53 +0530

    basic and practice 1 2 3

---
+++ Revision3/ch1_3.c
@@ -0,0 +1,30 @@
+#include<stdio.h> //& library
+
+void program() {
+    int sum; //& int,char and more are defination
+    int a; //& a is variable
+    int b = 5; //&  5 is constant
+    printf("Whats a: ");
+    scanf("%d",&a); //&  & means address of
+
+
+    sum = a+b;
+    printf("sum of %d and %d is %d", a,b,sum); 
+}
+
+void program1() {
+    int a;
+    int b;
+
+    printf("Whats length: ");
+    scanf("%d", &a);
+    printf("Whats Breadth: ");
+    scanf("%d", &b);
+    
+    printf("Area of rectangle is %d", a*b );
+}
+
+int main() {
+    program1();
+    return 0;
+}
