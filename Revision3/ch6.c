28a6b2ec63399e99c83b855e816cacc877783dd0
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-06 23:16:13 +0530

    added ques on pointer

---
+++ Revision3/ch6.c
@@ -0,0 +1,40 @@
+/*
+Write a C program that takes two integers and uses pointers to:
+
+Print their values
+Print their addresses
+Find the larger number using pointer dereferencing
+Swap the two numbers using pointers
+
+Requirement: Perform the swap through a separate swap() function using pointers.*/
+
+#include<stdio.h>
+
+
+void function1() {
+    int i;
+    int n;
+    int *k;
+    k = &n;
+    int *j;
+    j = &i;
+
+    printf("Whats value of i: \n");
+    scanf("%d", &i);
+
+    printf("Whats value of n: \n");
+    scanf("%d", &n);
+
+    printf("\nYour i value is %d",i);
+    printf("\nYour n value is %d",n);
+    printf("\nYour i location is %d",j);
+    printf("\nYour n location is %d",k);
+    swap(&i, &n);
+    printf("\nYour n location is %d", n);
+    
+}
+
+int main() {
+    function1();
+    return 0;
+}
