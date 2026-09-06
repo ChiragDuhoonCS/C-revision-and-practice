18e9eb6c29ecb974ce27371e1abcc8e9b61523eb
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-06 23:32:34 +0530

    add code

---
+++ Revision3/ch7.c
@@ -0,0 +1,27 @@
+/*
+Write a C program that takes 10 integers in an array and:
+
+Print all elements
+Find the largest and smallest element
+Calculate the sum and average
+Search for a number entered by the user and print its index if found
+
+Requirement: Use an array and loops. No separate functions.*/
+
+#include<stdio.h>
+
+void function1() {
+    int n;
+    int arr[10];
+
+    for (int i = 0; i < n; i++)
+    {
+        printf(arr[i]);
+    }
+    
+}
+
+int main() {
+    function1();
+    return 0;
+}
