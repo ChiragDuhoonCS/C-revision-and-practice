5ebfd054922e29d2bd252625792f08b0e1732f48
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-08 23:10:24 +0530

    chapter 1 to 5

---
+++ REVISION2/chapter1_to_5.c
@@ -0,0 +1,20 @@
+//& REVISION CHAPTER 1 TO 5
+
+/*%----- NUMBER ANALYSIS -----
+
+1. Count digits
+2. Reverse number
+3. Check palindrome
+4. Check Armstrong
+5. Print factors
+6. Check prime
+7. GCD
+8. LCM
+9. Multiplication table
+10. Back to Main Menu*/
+
+#include<stdio.h>
+
+void print1() {
+    
+}
