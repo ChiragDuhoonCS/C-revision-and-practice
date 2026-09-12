f8210b18b4aa38756a7037740e369b35edbcd690
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-12 21:43:35 +0530

    input

---
+++ CHAPTER1/input.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    // scanf to get input from user
+    scanf("%d", &a);
+    // & means adress of
+    // %d means placeholder of user input 
+    /* %f %c for float for character respectively 
+    */
+    printf("the value of this is %d", a);
+    return 0 ;
+}
