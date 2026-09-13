528ec16cab6c6ff93a2656373e28bd96a2e3f513
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-13 23:26:02 +0530

    practice

---
+++ CHAPTER1/variable.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int a = 6;
+    // tung tung sahur
+    printf("The output of this answer is %d", a);
+    /* this is for multiple
+    lines also this is called comment*/
+    return 0;
+}
