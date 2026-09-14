a1c3a1f90750b584f5c3c7d4f42c908462bb639b
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-14 22:37:31 +0530

    practice

---
+++ CHAPTER2/2practice_REMAINDER.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+// to find remaider 
+
+int main() {
+    int a;
+
+    printf("put value\n");
+    scanf("%d", &a);
+
+    printf("the remainder of 97 is %d", 97 % a); // here see how i write 
+    return 0;
+}
