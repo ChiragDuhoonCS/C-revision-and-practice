4d3130041cea72611152563f41ae1fea55e862fd
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-19 22:32:52 +0530

    control loop

---
+++ CHAPTER4_LOOP/CONTROL_LOOP.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+
+    printf("What's n?\n");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+       printf ("meow\n");
+    }
+    return 0;
+    
+}
