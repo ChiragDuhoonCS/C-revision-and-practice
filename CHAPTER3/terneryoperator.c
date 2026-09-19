83b769a9b7fd6c318aa091ca0b4a752f8829e1f1
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-19 22:32:14 +0530

    practice

---
+++ CHAPTER3/terneryoperator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+    int a = 454;
+    int b = 886;
+    // condition ? expression-if-true : expression-if-false
+    a>b?printf("a is greater"):printf("b is greater");
+    return 0;
+
+}
