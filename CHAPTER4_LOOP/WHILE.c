acc7790c5a75906d7eff72c5894566068eb3497e
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-22 22:08:45 +0530

    while

---
+++ CHAPTER4_LOOP/WHILE.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int i = 0;  // its like 0 1 2 
+    while (i < 3)  // for 2 1 0   i = 3  i--        // for unlimited loop while(true)
+    {
+        printf("Meow\n");
+        i++;
+    }
+    return 0;
+}
